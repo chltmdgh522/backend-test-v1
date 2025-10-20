5ea7b8e445aa99d2ea24d9db0405bb818401f5e5
chltmdgh522 <chltmdgh517@naver.com>
2025-10-20 22:37:37 +0900

    [Docs]: SwaggerConfig에 Production 서버 URL 추가
    - @OpenAPIDefinition 어노테이션을 추가하여 서버 정보 명시

--- modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/config/SwaggerConfig.kt
+++ modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/config/SwaggerConfig.kt
@@ -1,9 +1,10 @@
 package im.bigs.pg.api.config
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.info.License
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,6 +12,11 @@ import org.springframework.context.annotation.Configuration
  * Swagger/OpenAPI 문서화를 위한 설정 클래스.
  */
 @Configuration
+@OpenAPIDefinition(
+    servers = [
+        Server(url = "https://flow.madras.p-e.kr", description = "Production Server")
+    ]
+)
 class SwaggerConfig {
 
     @Bean
@@ -28,10 +34,5 @@ class SwaggerConfig {
                 .name("나노바나나 페이먼츠")
                 .url("https://example.com")
                 .email("contact@example.com")
-        )
-        .license(
-            License()
-                .name("Private License")
-                .url("https://example.com/license")
         )
 }
