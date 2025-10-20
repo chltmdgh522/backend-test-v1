9e45b5c0c307344dcf5fd74854b603bd2c3e8513
chltmdgh522 <chltmdgh517@naver.com>
2025-10-20 22:34:01 +0900

    [Docs]: ErrorResponse 예시 상태코드 400 → 500으로 수정

--- modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/payment/dto/ErrorResponse.kt
+++ modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/payment/dto/ErrorResponse.kt
@@ -7,7 +7,7 @@ data class ErrorResponse(
     @Schema(description = "발생 시각", example = "2025-10-20T13:27:50.029+00:00")
     val timestamp: String,
 
-    @Schema(description = "HTTP 상태 코드", example = "400")
+    @Schema(description = "HTTP 상태 코드", example = "500")
     val status: Int,
 
     @Schema(description = "에러 요약", example = "Bad Request")
